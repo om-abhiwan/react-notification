e6c068c2a60e796f1dc40d808f2dad0ab51dc883
om-abhiwan <om.abhiwan@gmail.com>
2024-05-14 19:01:03 +0530

    Code Updated for notifiation permission

--- src/App.js
+++ src/App.js
@@ -1,69 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import { ToastContainer, Zoom } from 'react-toastify';
+import Notification from './firebaseNotifications/Notification';
 import logo from './logo.svg';
 import './App.css';
-import { ToastContainer, Zoom } from "react-toastify";
-import Notification from "./firebaseNotifications/Notification";
-import { pushMessage } from './firebaseNotifications/pushNotification';
-import { useEffect, useState } from 'react';
-
-
-
 
 function App() {
-
-
   useEffect(() => {
-    window.addEventListener('load', function () {
-      if (!('Notification' in window)) {
-        console.log('No Push API Support!')
-      } else {
-        window.Notification.requestPermission().then(function (permission) {
-          if (permission === "granted") {
-            // alert("granted")
-            console.log("Permission granted for Notification..")
-          } else if (permission === "denied") {
-            // If permission is denied, display a message on the screen
-            console.log("Permission denied for Notification...");
-            // alert("denied")
-            document.body.insertAdjacentHTML('beforebegin', '<p style="color:red;text-align:center;">Notification permission denied. Please allow notification permission to use this feature.</p>');
-          } else {
-            // If permission is neither granted nor denied, request permission again
-            window.Notification.requestPermission().then(function (permission) {
-              if (permission === "granted") {
-                console.log("Permission granted for Notification..")
-                // alert("granted2")
-              } else if (permission === "denied") {
-                // alert("denied2")
-                // If permission is denied, display a message on the screen
-                console.log("Permission denied for Notification...");
-                document.body.insertAdjacentHTML('beforebegin', '<p style="color:red; text-align:center;">Notification permission denied. Please allow notification permission to use this feature.</p>');
-              }
-            });
-          }
-        });
-      }
-    });
+    // Request notification permission when the component mounts
+    requestNotificationPermission();
   }, []);
-
-
-
-
-
-
-  // useEffect(() => {
-  //   const intervalId = setInterval(() => {
-  //     pushMessage();
-  //   }, 10000); // 5000 milliseconds = 5 seconds
-
-  //   return () => {
-  //     clearInterval(intervalId);
-  //   };
-  // }, []);
 
   const [copyMessage, setCopyMessage] = useState('');
 
   const myFunction = () => {
     const fcmKey = document.getElementById('fcmKey').innerText;
-    navigator.clipboard.writeText(fcmKey)
+    navigator.clipboard
+      .writeText(fcmKey)
       .then(() => {
         setCopyMessage('Text copied to clipboard!');
         setTimeout(() => {
@@ -79,52 +31,49 @@ function App() {
       });
   };
 
-
-
-
-
-
-
+  const requestNotificationPermission = () => {
+    if ('Notification' in window) {
+      // Check if the browser supports notifications
+      window.Notification.requestPermission().then(permission => {
+        if (permission === 'granted') {
+          console.log('Permission granted for Notification..');
+        } else if (permission === 'denied') {
+          console.log('Permission denied for Notification...');
+          document.body.insertAdjacentHTML(
+            'beforebegin',
+            '<p style="color:red;text-align:center;">Notification permission denied. Please allow notification permission to use this feature.</p>'
+          );
+        }
+      });
+    } else {
+      console.log('No Push API Support!');
+    }
+  };
 
   return (
-
-
-    <>
-      <div className="App">
-        <ToastContainer
-          position="bottom-center"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={true}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          transition={Zoom}
-          closeButton={false}
-        />
-        <Notification />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            v.0 <code>src/App.js</code> and save to reload.
-          </p>
-
-          <p>FCM KEY :- <span id='fcmKey'>{localStorage.getItem('fcmToken')}</span>  </p>
-          <button onClick={myFunction}>Copy text</button>
-          <p>{copyMessage && <p>{copyMessage}</p>}</p>
-
-        </header>
-      </div>
-
-
-
-
-    </>
-
-
-
+    <div className="App">
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={true}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        transition={Zoom}
+        closeButton={false}
+      />
+      <Notification />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>v.0 <code>src/App.js</code> and save to reload.</p>
+        <p>FCM KEY :- <span id="fcmKey">{localStorage.getItem('fcmToken')}</span></p>
+        <button onClick={myFunction}>Copy text</button>
+        <p>{copyMessage && <p>{copyMessage}</p>}</p>
+      </header>
+    </div>
   );
 }
 
